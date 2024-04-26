b729425df840033da02c1b858b533d2abe486b71
sirlyun <sirlyun23@gmail.com>
2024-04-26 17:17:46 +0900

    Fix: prefix  오류 수정

--- backend/src/main/java/com/vita/backend/character/domain/Character.java
+++ backend/src/main/java/com/vita/backend/character/domain/Character.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
