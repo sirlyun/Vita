dc309b5a7713b791bd64e0abcb9126b6406e25bc
sirlyun <sirlyun23@gmail.com>
2024-04-28 18:38:54 +0900

    Fix: RestController 추가

--- backend/src/main/java/com/vita/backend/global/controller/TestController.java
+++ backend/src/main/java/com/vita/backend/global/controller/TestController.java
@@ -3,7 +3,9 @@ package com.vita.backend.global.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/test")
 public class TestController {
 	@GetMapping("/health")
