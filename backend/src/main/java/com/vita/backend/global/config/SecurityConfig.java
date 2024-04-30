2564888cb93c58d486d43d5c5af82e530360c4f8
박철준 <dizncjfwns@nate.com>
2024-04-30 17:46:54 +0900

    Chore: SecurityConfig 주석 해제

--- backend/src/main/java/com/vita/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/vita/backend/global/config/SecurityConfig.java
@@ -1,47 +1,87 @@
-//package com.vita.backend.global.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        //csrf disable
-//        http
-//                .csrf((auth) -> auth.disable());
-//
-//        //From 로그인 방식 disable
-//        http
-//                .formLogin((auth) -> auth.disable());
-//
-//        //HTTP Basic 인증 방식 disable
-//        http
-//                .httpBasic((auth) -> auth.disable());
-//
-//        //oauth2
-//        http
-//                .oauth2Login(Customizer.withDefaults());
-//
-//        //경로별 인가 작업
-//        http
-//                .authorizeHttpRequests((auth) -> auth
-//                        .anyRequest().permitAll());
-//
-//        //세션 설정 : STATELESS
-//        http
-//                .sessionManagement((session) -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//        return http.build();
-//    }
-//
-//}
+package com.vita.backend.global.config;
+
+import com.vita.backend.jwt.JWTFilter;
+import com.vita.backend.jwt.JWTUtil;
+import com.vita.backend.member.handler.LoginSuccessHandler;
+import com.vita.backend.member.service.MemberServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final MemberServiceImpl memberService;
+    private final LoginSuccessHandler loginSuccessHandler;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(MemberServiceImpl memberService, LoginSuccessHandler loginSuccessHandler, JWTUtil jwtUtil) {
+        this.memberService = memberService;
+        this.loginSuccessHandler = loginSuccessHandler;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // CORS
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    configuration.setAllowedMethods(Collections.singletonList("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+                    configuration.setMaxAge(3600L);
+                    configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+                    return configuration;
+                }));
+
+        // csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        // From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        // HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // JWTFilter 추가
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        // OAuth2
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig
+                                .baseUri("/member/oauth2/authorization")) // 엔드포인트 변경
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(memberService))
+                        .successHandler(loginSuccessHandler));
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().permitAll());
+
+        // 세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}
