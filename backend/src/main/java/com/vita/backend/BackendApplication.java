b53b38ebe7ad44a576523d28ed75ca6474a20680
박철준 <dizncjfwns@nate.com>
2024-04-30 17:51:16 +0900

    Chore: BaseEntity auditing 애노테이션 추가

--- backend/src/main/java/com/vita/backend/BackendApplication.java
+++ backend/src/main/java/com/vita/backend/BackendApplication.java
@@ -2,9 +2,12 @@ package com.vita.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableMongoRepositories
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
