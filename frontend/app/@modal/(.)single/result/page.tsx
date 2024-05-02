b90080e7faab20c0f59be8e5220f952c158e26f9
MuRing <11esh@naver.com>
2024-05-02 17:58:21 +0900

    Feat: new best record update

--- frontend/app/@modal/(.)single/result/page.tsx
+++ frontend/app/@modal/(.)single/result/page.tsx
@@ -1,28 +1,34 @@
 "use client";
 
-import useUserStore from "@/store/user-store";
 import { Modal } from "../../modal";
-
-import Link from "next/link";
+import useUserStore from "@/store/user-store";
 import styles from "@/public/styles/modal.module.scss";
 
 export default function SingleResult() {
   const userStore = useUserStore();
-  console.log(userStore.gameType);
-  console.log(userStore.record);
+  let isNewRecord = false;
+
+  if (userStore.bestRecord < userStore.record) {
+    isNewRecord = true;
+    userStore.bestRecord = userStore.record;
+  }
+
   return (
     <div>
       <Modal>
-        <p className={`${styles.title} ${styles.center}`}>result page modal</p>
+        <p className={`${styles.title} ${styles.center}`}>RESULT</p>
         {userStore.gameType === 0 ? (
-          <div>
-            <p>Game Type 0: This is the result for game type 0.</p>
+          <div className={styles.content}>
+            <p>달리기</p>
             <p>Your record: {(userStore.record / 1000).toFixed(3)}초</p>
+            {isNewRecord && <p className={styles.highlight}>최고 기록 갱신!</p>}
           </div>
         ) : userStore.gameType === 1 ? (
-          <div>
-            <p>Game Type 1: This is the result for game type 1.</p>
-            <p>Your record: {userStore.record}</p>
+          <div className={styles.content}>
+            <p>10초안에 아령 존나 하기</p>
+            <p>나의 기록: {userStore.record}</p>
+            <p>나의 최고 기록: {userStore.bestRecord}</p>
+            {isNewRecord && <p className={styles.highlight}>최고 기록 갱신!</p>}
           </div>
         ) : (
           <div>
