8d42c6f4d0153c458fa71ec01bac46ffd8639e21
김성수 <zxzxzlxlzlxl@naver.com>
2024-05-10 09:53:01 +0900

    Refactor: 회원 정보 수정 코드 리팩터링

--- frontend/app/(router)/login/page.tsx
+++ frontend/app/(router)/login/page.tsx
@@ -2,12 +2,11 @@
 
 import styles from "@/public/styles/login.module.scss";
 import LoginComponent from "@/components/login/page";
-import Chronic from "@/components/member/page";
 
 export default function Login() {
   return (
     <div className={`${styles.main} background`}>
-      <Chronic />
+      <LoginComponent />
     </div>
   );
 }
