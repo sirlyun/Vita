103761645b47765312aad68b2fb76e22fdaf0292
MuRing <11esh@naver.com>
2024-05-13 15:34:08 +0900

    Feat: interface added

--- frontend/app/(router)/page.tsx
+++ frontend/app/(router)/page.tsx
@@ -11,51 +11,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-// Enum for Gender and Body Shape
-enum Gender {
-  MALE = "MALE",
-  FEMALE = "FEMALE",
-}
-
-enum BodyShape {
-  SKINNY = "SKINNY",
-  NORMAL = "NORMAL",
-  FAT = "FAT",
-}
-
-// Interface for Achievement
-interface Achievement {
-  level: number;
-  name: string;
-}
-
-// Interface for Debuff
-interface DeBuff {
-  de_buff_id: number;
-  type: string;
-  vita_point: number;
-}
-
-// Interface for Character Item
-interface CharacterItem {
-  my_item_id: number;
-  type: string;
-  name: string;
-}
-
-// Main Character Interface
-interface Character {
-  character_id: number;
-  nickname: string;
-  vita_point: number;
-  is_dead: boolean;
-  gender: Gender;
-  body_shape: BodyShape;
-  achievement: Achievement;
-  de_buff: DeBuff[];
-  character_item: CharacterItem[];
-}
-
 export default function Home() {
   const [challengeModal, setChallengeModal] = useState(false);
   const [userInfo, setUserInfo] = useState<Character | null>(null);
